d29a2bc8a43742e5a63b0818250330baf1856b48
Eldar <elogon@gmail.com>
2021-03-15 17:00:36 -0400

    Add axios semantic-ui react-router dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import JOBTrackLogo from "./assets/images/jobTrack-logo.png"
 import "./App.css";
 
