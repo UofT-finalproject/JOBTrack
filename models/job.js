6781cf4d6df4ce4f117c159bf08576c75cb4a11f
Eldar <elogon@gmail.com>
2021-03-16 12:55:45 -0400

    Create mongodb schema for Job and seeder file

---
+++ models/job.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const jobSchema = new Schema({
+  searchId: String,
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  type: String,
+  location: String,
+  company: String,
+  url: String,
+  created_at: String,
+  applied: { type: Boolean, default: false },
+  date_applied: Date,
+  status: { type: String, enum: ['None', 'Applied', 'Interviewed', 'Approved', 'Archived']},
+  attachments: String,
+  date: { type: Date, default: Date.now }
+});
+
+const Job = mongoose.model("Job", jobSchema);
+
+module.exports = Job;
