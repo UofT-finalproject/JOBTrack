0a00dc5f674b955f3bb91f6df4700985361b3428
Eldar <elogon@gmail.com>
2021-03-16 13:32:44 -0400

    Add momentjs and date display on navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu, Container, Icon } from 'semantic-ui-react';
-import logo from '../../assets/images/jobTrack-logo.png';
 import logoWhite from '../../assets/images/jobTrack-white.png';
+import moment from "moment";
 import './style.css';
 
 function Navbar() {
     const activeItem = 'Dashboard';
+    const [date, setDate] = useState(moment().format("LL"));
     const handleItemClick = (e, { name }) => console.log( name );
 
     return (
         <Menu stackable color={'grey'} inverted size='huge' >
         <Container>
           <Menu.Item>
-            <img src={logoWhite} size='huge' className='logo-nav' />
-          </Menu.Item>
-  
+            <img src={logoWhite} size='huge' className='logo-nav' alt='logo' />
+          </Menu.Item >
+          <Menu.Item position='right'>
+            {date}
+            </Menu.Item>
           <Menu.Item
             position='right'
             name='dashboard'
