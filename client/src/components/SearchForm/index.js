632c9da0070615e4a7a34241528895b243f94b03
Omair Muhi <omairmuhi@icloud.com>
2021-03-19 08:53:25 -0400

    Subject: Get job data from GH when radio is checked Problem: NA Solution: NA Note: NA

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -6,74 +6,81 @@ import { useStoreContext } from "../../utils/GlobalState";
 import './style.css'
 
 function SearchForm() {
-  const [state, dispatch] = useStoreContext();
-  // state = { title: '', location: '', fullTime: false }
-  const [search, setSearch] = useState({ title: '', location: '' });
-  const handleChange = (e, { name, value }) => setSearch({ ...search, [name]: value })
-  // radio button state
-  const [radioValue, setRadioValue] = useState('');
-  function handleRadioButtonChange(radioValue, e) {
-    e.preventDefault();
-    setRadioValue(radioValue);
-  }
+    const [state, dispatch] = useStoreContext();
+    // state = { title: '', location: '', fullTime: false }
+    const [search, setSearch] = useState({ title: '', location: '' });
+    const handleChange = (e, { name, value }) => setSearch({...search, [name]: value })
+        // radio button state
+    const [radioValue, setRadioValue] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (search) {
-      // setSearch({ title: title, location: location });
-      const { title, location } = search;
-      API.searchJobs(title, location)
-        .then(res => {
-          console.log(res.data);
-          dispatch({
-            type: SAVE_SEARCH,
-            searchedJobs: res.data
-          })
-        })
-        .catch(err => console.log(err));
+    function handleRadioButtonChange(radioValue, e) {
+        e.preventDefault();
+        setRadioValue(radioValue);
     }
-  }
 
-  const { title, location } = search;
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (search) {
+            // setSearch({ title: title, location: location });
+            const { title, location } = search;
+            API.searchJobs(title, location, radioValue)
+                .then(res => {
+                    console.log(res.data);
+                    dispatch({
+                        type: SAVE_SEARCH,
+                        searchedJobs: res.data
+                    })
+                })
+                .catch(err => console.log(err));
+        }
+    }
 
-  return (
-    <div>
-      <Grid centered>
-        <Form className='search-form' onSubmit={handleSubmit}>
-          <Form.Group widths='equal' >
-            <Form.Input
-              placeholder='Job Title'
-              name='title'
-              value={title}
-              onChange={handleChange}
-            />
-            <Form.Input
-              placeholder='Location'
-              name='location'
-              value={location}
-              onChange={handleChange}
-            />
-            <Form.Button content='Search' icon='search' />
-          </Form.Group>
-          <Form.Group inline>
-            <label>Job-board</label>
-            <Form.Radio
-              label='LinkedIn'
-              value='li'
-              checked={radioValue === 'li'}
-              onChange={(e) => handleRadioButtonChange('li', e)}
-            />
-            <Form.Radio
-              label='GitHub'
-              value='gh'
-              checked={radioValue === 'gh'}
-              onChange={(e) => handleRadioButtonChange('gh', e)}
-            />
-          </Form.Group>
-        </Form>
-      </Grid>
-    </div>
-  )
+    const { title, location } = search;
+
+    return ( <
+        div >
+        <
+        Grid centered >
+        <
+        Form className = 'search-form'
+        onSubmit = { handleSubmit } >
+        <
+        Form.Group widths = 'equal' >
+        <
+        Form.Input placeholder = 'Job Title'
+        name = 'title'
+        value = { title }
+        onChange = { handleChange }
+        /> <
+        Form.Input placeholder = 'Location'
+        name = 'location'
+        value = { location }
+        onChange = { handleChange }
+        /> <
+        Form.Button content = 'Search'
+        icon = 'search' / >
+        <
+        /Form.Group> <
+        Form.Group inline >
+        <
+        label > Job - board < /label> <
+        Form.Radio label = 'LinkedIn'
+        value = 'li'
+        checked = { radioValue === 'li' }
+        onChange = {
+            (e) => handleRadioButtonChange('li', e) }
+        /> <
+        Form.Radio label = 'GitHub'
+        value = 'gh'
+        checked = { radioValue === 'gh' }
+        onChange = {
+            (e) => handleRadioButtonChange('gh', e) }
+        /> <
+        /Form.Group> <
+        /Form> <
+        /Grid> <
+        /div>
+    )
 
 }
 
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,15 +3,20 @@ const BASE_API_URL = 'http://localhost:3000';
 
 export default {
     // Gets all saved jobs
-  getSavedJobs: function() {
-    return axios.get("/api/jobs");
-  },
+    getSavedJobs: function() {
+        return axios.get("/api/jobs");
+    },
 
-  searchJobs: function(title, location) {
-    const descriptionQuery = title ? `description=${title}`: '';
-    const locationQuery = location ? `&location=${location}` : '';
-    const query = `${BASE_API_URL}/api/search?${descriptionQuery}${locationQuery}`;
-    console.log(query);
-    return axios.get(query);
-  },
+    searchJobs: function(title, location, jobBoard) {
+        if (jobBoard === 'gh') {
+            const descriptionQuery = title ? `description=${title}` : '';
+            const locationQuery = location ? `&location=${location}` : '';
+            const query = `${BASE_API_URL}/api/search?${descriptionQuery}${locationQuery}`;
+            console.log(query);
+            return axios.get(query);
+        } else if (jobBoard === 'li') {
+            const query = "";
+            return axios.get(query);
+        }
+    },
 }
