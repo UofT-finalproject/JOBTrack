bcd605229c9d888a0ed5267a5f463e62b38a0a67
Ivan Duranic <ivan.duranic@gmail.com>
2021-03-20 22:38:27 -0400

    added JobDetail link in Dashboard file

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import JobsList from '../components/JobsList';
-import SideMenu from '../components/SideMenu';
-import { Grid } from 'semantic-ui-react';
+import React from "react";
+import JobsList from "../components/JobsList";
+import SideMenu from "../components/SideMenu";
+import JobDetail from "../components/JobDetail";
+import { Grid } from "semantic-ui-react";
 
 function Dashboard() {
-
-    return (
-        <div>
-            
-            <Grid columns='equal'>
-                <Grid.Row>
-                    <Grid.Column width={3}>
-                        <SideMenu />
-                    </Grid.Column>
-                    <Grid.Column width={12}>
-                        <JobsList />
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Grid columns="equal">
+        <Grid.Row>
+          <Grid.Column width={3}>
+            <SideMenu />
+          </Grid.Column>
+          <Grid.Column width={12}>
+            <JobsList />
+          </Grid.Column>
+        </Grid.Row>
+        <JobDetail />
+      </Grid>
+    </div>
+  );
 }
 
 export default Dashboard;
