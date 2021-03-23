2989e675ed32bc4d1be22acd7cad9c96eb1e45d9
Dazrin Tioseco <xdazee@gmail.com>
2021-03-23 12:44:55 -0700

    Creating a edit profile modal which appears onclick of edit profile button

--- client/src/components/EditProfileInfo/index.js
+++ client/src/components/EditProfileInfo/index.js
@@ -1,2 +1,64 @@
-// TODO:
-// Create edit profile component
+import React from 'react'
+import { Form, Button, Header, Icon, Image, Modal } from 'semantic-ui-react'
+const style = <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css'/>
+//TODO: finish creating edit modal and capture input to update ProfileInfo 
+const Edit = () => {
+    const [input, setInput] = useState(
+      {
+        firstname: '',
+        lastname: '',
+        about: '',
+        location: '',
+        company: '',
+        github: '',
+        created_at: moment().format('DD/MM/YYYY'),
+        applied: false ,
+        date_joined: '',
+        status: 'None',
+        notes: '',
+        attachments: '',
+      });
+
+const EditModal = () => {
+    return (
+    <Modal trigger={<Button>Edit Profile</Button>}>
+    <Modal.Header>Profile Picture</Modal.Header>
+    <Modal.Content image>
+      <Image wrapped size='medium' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+      <Modal.Description>
+        <Header>Modal Header</Header>
+        <p>This is an example of expanded content that will cause the modal's dimmer to scroll</p>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group>
+            <Form.Input
+              placeholder='Name'
+              name='name'
+              value={name}
+              onChange={this.handleChange}
+            />
+            <Form.Input
+              placeholder='Email'
+              name='email'
+              value={email}
+              onChange={this.handleChange}
+            />
+            <Form.Button content='Submit' />
+          </Form.Group>
+        </Form>
+        <strong>onChange:</strong>
+        <pre>{JSON.stringify({ name, email }, null, 2)}</pre>
+        <strong>onSubmit:</strong>
+        <pre>{JSON.stringify({ submittedName, submittedEmail }, null, 2)}</pre>
+
+      </Modal.Description>
+    </Modal.Content>
+    <Modal.Actions>
+      <Button primary>
+        Proceed <Icon name='right chevron' />
+      </Button>
+    </Modal.Actions>
+  </Modal>
+    )
+}
+
+export default () => (<div>{style}<EditModal/></div>)
