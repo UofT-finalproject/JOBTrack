b30fd9406b8958171b18510c95f3bb4d6692c3ab
Omair Muhi <omairmuhi@icloud.com>
2021-03-24 08:46:33 -0400

    Subject: Basic JEST setup Problem: NA Solution: NA Note: NA

---
+++ client/src/App.spec.js
@@ -0,0 +1,15 @@
+// describe('MUSE API call', () => {
+//     it('Check fields', () => {
+//         const params = ['Software Engineer', 'Toronto, Canada', 'li'];
+//         searchJobs(...params)
+//             .then(res => {
+//                 expect(res.data.total).toBeGreaterThan(0);
+//             })
+//     });
+// });
+
+describe('My Test Suite', () => {
+    it('My Test Case', () => {
+        expect(true).toEqual(true);
+    });
+});
