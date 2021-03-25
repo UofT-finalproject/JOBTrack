45c05da6fb813607f09c297f3b4bffbb6961e688
Omair Muhi <omairmuhi@icloud.com>
2021-03-24 22:25:36 -0400

    Subject: Group searchJobs() under one describe() Problem: NA Solution: NA Note: NA

--- client/src/tests/API.spec.js
+++ client/src/tests/API.spec.js
@@ -3,17 +3,14 @@
 // https://stackoverflow.com/questions/58613492/how-to-resolve-cannot-use-import-statement-outside-a-module-in-jest
 import API from '../utils/API';
 
-describe('test searchJobs with The Muse API', () => {
-    it('Check for time-out', () => {
+describe('Unit-tests for searchJobs()', () => {
+    it('Check for time-out with Muse API', () => {
         return API.searchJobs('Software Engineer', 'Toronto, Canada', 'li')
             .then(res => {
                 expect(res.data.timed_out).toBeFalsy();
             })
     });
-});
-
-describe('test searchJobs with GitHub API', () => {
-    it('Check for time-out', () => {
+    it('Check job ids are alphanumeric for GitHub API', () => {
         return API.searchJobs('Software Engineer', 'San Francisco', 'gh')
             .then(res => {
                 res.data.forEach((job) => {
