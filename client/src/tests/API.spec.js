7f5588c4314f7792d93ddbccf15e56aba6f09e14
Omair Muhi <omairmuhi@icloud.com>
2021-03-24 21:56:49 -0400

    Subject: Add unit-test for searchJobs() Problem: NA Solution: NA Note: Test time out on The Muse API

--- client/src/tests/API.spec.js
+++ client/src/tests/API.spec.js
@@ -1,11 +1,11 @@
 // To get import statments working see reply from
 // Natan Williams to this stackoverflow post:
 // https://stackoverflow.com/questions/58613492/how-to-resolve-cannot-use-import-statement-outside-a-module-in-jest
-import API from './utils/API';
+import API from '../utils/API';
 
 describe('test searchJobs with The Muse API', () => {
     it('Check for time-out', () => {
-        API.searchJobs('Software Engineer', 'Toronto', 'li')
+        return API.searchJobs('Software Engineer', 'Toronto, Canada', 'li')
             .then(res => {
                 expect(res.data.timed_out).toBeFalsy();
             })
