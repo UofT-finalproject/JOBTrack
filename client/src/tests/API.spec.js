8516bee7bfde88e3f691a0ebbcbce4a145dd0464
Omair Muhi <omairmuhi@icloud.com>
2021-03-25 18:57:13 -0400

    Subject: Add expect() for validating URL Problem: NA Solution: NA Note: NA

--- client/src/tests/API.spec.js
+++ client/src/tests/API.spec.js
@@ -11,20 +11,32 @@ describe('Unit-tests for searchJobs()', () => {
     it('Test searchJobs() with Muse API', () => {
         const museJob = [{ timed_out: true }];
         const resp = { data: museJob };
-        // use mocked axios module to return fake data
+        // use mock'd axios module to return fake data
         axios.get.mockResolvedValue(resp);
         return API.searchJobs('Software Engineer', 'Toronto, Canada', 'li')
             .then(res => {
+                const query = `/api/search/muse?category=Software Engineer&location=Toronto, Canada`;
+                // Validate URL passed to AXIOS
+                expect(axios.get).toHaveBeenCalledWith(
+                    query,
+                );
+                // Check that fake data is returned 
                 expect(res.data).toEqual(museJob);
             })
     });
-    it('Test searchJobs() with Muse API', () => {
+    it('Test searchJobs() with GitHub API', () => {
         const ghJob = [{ id: 'abc-123' }];
         const resp = { data: ghJob };
-        // use mocked axios module to return fake data
+        // use mock'd axios module to return fake data
         axios.get.mockResolvedValue(resp);
         return API.searchJobs('Developer', 'Toronto', 'gh')
             .then(res => {
+                const query = `/api/search?description=Developer&location=Toronto`;
+                // Validate URL passed to AXIOS
+                expect(axios.get).toHaveBeenCalledWith(
+                    query,
+                );
+                // Check that fake data is returned 
                 expect(res.data).toEqual(ghJob);
             })
     });
